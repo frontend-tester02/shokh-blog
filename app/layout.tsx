1d84b3751bb7cc3b3de294fd8777b07b4cce7e8e
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-08-02 03:57:16 +0900

    Add Navbar, Layout and Footer to blog site

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Inter, Crete_Round, Work_Sans } from "next/font/google";
+import "./globals.css";
+import { ChildProps } from "@/types";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+const inter = Inter({ subsets: ["latin"] });
+const creteRound = Crete_Round({
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: '--font-creteRound',
+})
+
+const workSans = Work_Sans({
+  weight: ['500', '600'],
+  subsets: ['latin'],
+  variable: '--fonst-workSans'
+})
+
+export const metadata: Metadata = {
+  title: "Sammi dasturlashga oid maqolalar",
+  description: "Dasturlash haqida yangiliklar, maslahatlar va dasturlash sohasidagi eng so'nggi xabarlar. Bizning blogda dasturlashni o'rganish va rivojlantirish uchun qo'llanma topishingiz mumkin.",
+};
+
+function RootLayout({children} : ChildProps) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${creteRound.variable} ${workSans.variable} overflow-x-hidden`}>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        >{children}
+        </ThemeProvider>
+        </body>
+    </html>
+  );
+}
+
+
+export default RootLayout
