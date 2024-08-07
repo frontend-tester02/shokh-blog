aef544438481a84edf0623c7ee328e1b4facba00
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-08-08 00:15:46 +0500

    Updated async funtion  the homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,13 @@
 import BlogCard from "@/components/cards/blog"
 import BgArrow from "@/components/shared/bg-arrow"
 import { blogs } from "@/constants"
+import { getBlogs } from "@/service/blog.service"
 
-const HomePage = () => {
+async function HomePage() {
+    const blogs = await getBlogs()
+    
+    
+    
     return (
         <div className="max-w-6xl mx-auto">
             <div className="relative min-h-[60vh] flex items-center justify-center">
