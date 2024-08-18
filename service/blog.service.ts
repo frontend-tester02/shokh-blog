143d646fd772c47f53a02c054d7af88cc127eaed
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-08-18 09:55:36 +0500

    Add getArchiveBlogs service

--- service/blog.service.ts
+++ service/blog.service.ts
@@ -1,4 +1,4 @@
-import { IBlog } from "@/types"
+import { IArchiveBlog, IBlog } from "@/types"
 import request, { gql } from "graphql-request"
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT!
@@ -6,7 +6,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT!
 export const getBlogs = async () => {
     const query = gql`
         query MyQuery {
-            blogs {
+            blogs(where: {archive: false}) {
                 title
                 createdAt
                 author {
@@ -35,7 +35,7 @@ export const getBlogs = async () => {
         }
     `
 
-    const {blogs} = await request<{blogs: IBlog[]}>(graphqlAPI, query)
+    const {blogs} = await request<{blogs: IBlog[]}>(graphqlAPI, query, )
 
     return blogs
 }
@@ -91,4 +91,32 @@ export const getSearchBlogs = async (title: string) => {
     const {blogs} = await request<{blogs: IBlog[]}>(graphqlAPI, query, {title})
 
     return blogs
+}
+
+export const getArchiveBlogs = async () => {
+    const query = gql`
+        query MyQuery {
+            blogs(where: {archive: true}) {
+                title
+                createdAt
+                slug
+            }
+        }
+    `
+
+
+    const {blogs} = await request<{blogs: IBlog[]}>(graphqlAPI, query,)
+
+    const filteredBlogs= blogs.reduce((acc: {[year: string] : IArchiveBlog}, blog: IBlog) => {
+        const year = blog.createdAt.substring(0, 4);
+        if(!acc[year]) {
+            acc[year] = {year, blogs: []};
+        }
+        acc[year].blogs.push(blog)
+        return acc
+    }, {})
+    
+    const results: IArchiveBlog[] = Object.values(filteredBlogs)
+
+    return results
 }
